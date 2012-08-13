c68f393dcf0798c3b065e320174f415323704ac4
Yushun <sonnyhe2002@gmail.com>
2012-08-13 01:05:18 -0400

    change search to center with add button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
   def home
     session["user"] = User.first
-    @images = Dir.glob('public/images/*')
-    @welcome_image = "/welcome_pic.jpeg"
+    @images = Dir.glob('public/images/background/*')
+    @welcome_image = "/images/welcome_pic.jpeg"
+    @create_image = "/images/create.png"
     @search_text = 'Search'
     @inspiras = Inspira.find_with_reputation(:votes, :all, order: "votes desc")
   end
